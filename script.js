d4fbbc2f661f2ea751c3e7de24958413f487f40c
arjun <arjunputhli00@gmail.com>
2021-12-24 11:59:49 +0530

    completed hover painting feature

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let gridContainer = document.getElementById("gridContainer");
-for (let i = 1; i<=240; i++) {
-    let cell = document.createElement('div');
-    cell.style.width = "30px";
-    cell.style.height = "30px";
-    cell.style.border = "1px solid #000000";
-    gridContainer.appendChild(cell);
+id_number = '';
+let cell = [];
+
+for (let i = 0; i<=239; i++) {
+    cell[i] = document.createElement('div');
+    cell[i].classList.add('square')
+    cell[i].id = `cell${i}`;
+    cell[i].style.width = "30px";
+    cell[i].style.height = "30px";
+    cell[i].style.border = "1px solid #ffffff";
+    gridContainer.appendChild(cell[i]);
 }
 
+const cells = document.querySelectorAll('.square');
+cells.forEach((square) => {
+    square.addEventListener('mousemove', ()=>{
+        square.style.backgroundColor = 'black';
+    });
+});
