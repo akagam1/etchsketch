135c018105341bcc87d3d77d8d043ece8ebf0fd8
arjun <arjunputhli00@gmail.com>
2021-12-24 17:53:55 +0530

    added resize grid feature

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 let gridContainer = document.getElementById("gridContainer");
+let cellSize = document.querySelector("#select1");
 id_number = '';
 let cell = [];
+size = cellSize.value;
+gridLoop = (480/size)*(480/size) - 1 ;
+gridContainer.style.setProperty('grid-template-columns', 'repeat(auto-fill, '+size+'px)');
 
 //creates the grid
-for (let i = 0; i<=239; i++) {
-    cell[i] = document.createElement('div');
-    cell[i].classList.add('square')
-    cell[i].id = `cell${i}`;
-    cell[i].style.width = "30px";
-    cell[i].style.height = "30px";
-    cell[i].style.border = "1px solid #ffffff";
-    gridContainer.appendChild(cell[i]);
+function drawGrid(loop) {
+    for (let i = 0; i<=loop; i++) {
+        cell[i] = document.createElement('div');
+        cell[i].classList.add('square')
+        cell[i].id = `cell${i}`;
+        cell[i].style.border = "1px solid #ffffff";
+        gridContainer.appendChild(cell[i]);
+    }
+    sketch();
 }
 
-//coloring the cells
-const cells = document.querySelectorAll('.square');
-cells.forEach((square) => {
+function sketch(){
+    let cells = document.querySelectorAll('.square');
+    cells.forEach((square) => {
     square.addEventListener('mouseover', ()=>{
             square.style.backgroundColor = 'black';
     });
 });
+}
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+drawGrid(gridLoop);
+
+cellSize.addEventListener('change', () => {
+    gridContainer.style.setProperty('grid-template-columns', 'repeat(auto-fill, '+cellSize.value+'px)');
+    removeAllChildNodes(gridContainer);
+    let loops = (480/cellSize.value)*(480/cellSize.value) - 1;
+    drawGrid(loops);
+});
+
+//coloring the cells
+
+
+
+
+
+
+//adjust grid size
 //add eraser and pen tool
 //add choose your color option
 //clear option
